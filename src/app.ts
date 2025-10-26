4844833cd717fedbdcd046cdd94bb2ae5e4e7b89
Yurii Kozlov <yurka22870@gmail.com>
2025-10-26 14:21:19 +0200

    chore: setup CORS configuration

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
 import type { Request, Response } from 'express';
 
+import cors from 'cors';
 import express from 'express';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: '16kb'}));
+app.use(express.static('public'));
+app.use(cors({
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  origin: process.env.CORS_ORIGIN?.split(',') ?? 'http://localhost:5173'
+}));
 
 app.get('/test', (_req: Request, res: Response) => {
   res.json({ message: 'Welcome to the Express + TypeScript Server!' });
