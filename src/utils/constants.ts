abf1c6c6ba3a922d4e853cef51f366a73226f11a
Yurii Kozlov <yurka22870@gmail.com>
2025-10-26 18:55:49 +0200

    chore: add user role and task status enums

---
+++ src/utils/constants.ts
@@ -0,0 +1,11 @@
+export enum TaskStatus {
+  DONE = 'done',
+  IN_PROGRESS = 'in_progress',
+  TODO = 'todo'
+}
+
+export enum UserRole {
+  ADMIN = 'admin',
+  MEMBER = 'member',
+  PROJECT_ADMIN = 'project_admin'
+}
