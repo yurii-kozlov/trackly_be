f6c2b2d03021c7517461db79548473d90038af37
Yurii Kozlov <yurka22870@gmail.com>
2025-10-26 11:49:24 +0200

    chore: add express.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import dotenv from 'dotenv';
+import express from 'express';
 
 dotenv.config({
   path: './.env',
 })
 console.log('wasssup');
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.get('/test', (req, res) => {
+  res.send('Hello!');
+});
+
+app.listen(PORT, () => {
+  console.log(`The server is running on port: ${PORT}`);
+});
