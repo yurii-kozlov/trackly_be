17020dd9e66247aa8f92b5d7ed0a15dfb06a572d
Yurii Kozlov <yurka22870@gmail.com>
2025-10-26 12:33:24 +0200

    chore: integrate eslint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import type { Request, Response } from 'express';
+
 import dotenv from 'dotenv';
 import express from 'express';
-import type { Request, Response } from 'express';
+
 
 dotenv.config({
   path: './.env',
@@ -8,12 +10,12 @@ dotenv.config({
 console.log('wasssup');
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT ?? 8000;
 
 app.get('/test', (_req: Request, res: Response) => {
   res.json({ message: 'Welcome to the Express + TypeScript Server!' });
 });
 
 app.listen(PORT, () => {
-  console.log(`The server is running on port: ${PORT}`);
+  console.log(`The server is running on port: ${PORT as string}`);
 });
