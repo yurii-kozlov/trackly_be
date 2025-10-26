b8dfa78b45f1288123b814a637e83e0e02404380
Yurii Kozlov <yurka22870@gmail.com>
2025-10-26 12:23:36 +0200

    chore: integrate typescript

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import type { Request, Response } from 'express';
 
 dotenv.config({
   path: './.env',
@@ -9,8 +10,8 @@ console.log('wasssup');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.get('/test', (req, res) => {
-  res.send('Hello!');
+app.get('/test', (_req: Request, res: Response) => {
+  res.json({ message: 'Welcome to the Express + TypeScript Server!' });
 });
 
 app.listen(PORT, () => {
