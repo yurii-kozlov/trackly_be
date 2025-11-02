1c2dfca146b5833d534d1d2103dd662299eea66e
Yurii Kozlov <yurka22870@gmail.com>
2025-11-02 20:10:23 +0200

    feat: add new auth routes

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,13 +1,32 @@
 import { authController } from '#controllers/auth/auth.controllers.js';
 import { verifyJWT } from '#middlewares/auth.middleware.js';
 import { validate } from '#middlewares/validator.middleware.js';
-import { userLoginValidator, userRegisterValidator } from '#validators/auth.validators.js';
+import { userChangeCurrentPasswordValidator, userForgotPasswordValidator, userLoginValidator, userRegisterValidator, userResetForgotPasswordValidator } from '#validators/auth.validators.js';
 import { Router } from 'express';
 
 const router = Router();
 
 router.route('/register').post(userRegisterValidator(), validate, authController.registerUser);
 router.route('/login').post(userLoginValidator(), validate, authController.loginUser);
+router.route('/verify-email/:verificationToken').get(authController.verifyEmail);
+router.route('/refresh-token').post(authController.refreshAccessToken);
+router
+  .route('/forgot-password')
+  .post(userForgotPasswordValidator(), validate, authController.initiatePasswordReset);
+router
+  .route('/reset-password/:resetToken')
+  .post(userResetForgotPasswordValidator(), validate, authController.resetForgotPassword);
+
 router.route('/logout').post(verifyJWT, authController.logoutUser);
+router.route('/current-user').get(verifyJWT, authController.getCurrentUser);
+router
+  .route('/change-password')
+  .post(
+    verifyJWT,
+    userChangeCurrentPasswordValidator(),
+    validate,
+    authController.changeCurrentPassword,
+  );
+router.route('/resend-email-verification').post(verifyJWT, authController.resendEmailVerification);
 
 export default router;
--- src/validators/auth.validators.ts
+++ src/validators/auth.validators.ts
@@ -32,4 +32,31 @@ const userLoginValidator = (): ValidationChain[] => {
   ];
 };
 
-export { userLoginValidator, userRegisterValidator };
+const userChangeCurrentPasswordValidator = () => {
+  return [
+    body('oldPassword').notEmpty().withMessage('Old password is required'),
+    body('newPassword').notEmpty().withMessage('New password is required'),
+  ];
+};
+
+const userForgotPasswordValidator = () => {
+  return [
+    body('email')
+      .notEmpty()
+      .withMessage('Email is required')
+      .isEmail()
+      .withMessage('Email is invalid'),
+  ];
+};
+
+const userResetForgotPasswordValidator = () => {
+  return [body('newPassword').notEmpty().withMessage('Password is required')];
+};
+
+export {
+  userChangeCurrentPasswordValidator,
+  userForgotPasswordValidator,
+  userLoginValidator,
+  userRegisterValidator,
+  userResetForgotPasswordValidator,
+};
